e2fde8cf97ac184d260f3bbec42f7064ce1fa165
SJ.H <comcxx11@gmail.com>
2018-09-22 22:07:41 +0900

    Initial Commit

---
+++ GradientBackground/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GradientBackground
+//
+//  Created by 홍서진 on 2018. 9. 22..
+//  Copyright © 2018년 홍서진. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
